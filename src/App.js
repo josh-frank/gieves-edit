e9bc223150b28f8ba0e5aebfb1026d1ea7c731d1
Josh Frank <eurosewusa@gmail.com>
2021-05-19 12:15:02 -0400

    adjusting artboardPosition props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-// import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import Artboard from './components/Artboard';
 
 function App() {
+
+  const [ artboardPosition, setArtboardPosition ] = useState( {
+    zoom: 1.0,
+    offsetX: 50.0,
+    offsetY: 50.0
+  } );
 
   // const handleClick = useCallback( clickEvent => {
   //   console.log( `x:${ clickEvent.clientX } y:${ clickEvent.clientY }` );
@@ -13,11 +19,21 @@ function App() {
   //   return () => window.removeEventListener( "click", handleClick );
   // }, [ handleClick ] );
 
+  // const handleScroll = useCallback( scrollEvent => {
+  //   console.log('scrollEvent: ', scrollEvent);
+  // }, [] );
+
+  // useEffect( () => {
+  //   window.addEventListener( "scroll", handleScroll );
+  //   return () => window.removeEventListener( "scroll", handleScroll );
+  // }, [ handleScroll ] );
+
   return (
     <>
       <Artboard
-        width="300"
-        height="300"
+        width={ 300 }
+        height={ 300 }
+        position={ artboardPosition }
         shapes={ [
           "m 0 0 l 50 0 l 0 50 l -50 0 l 0 -50",
           "m 150, 150 a 25,25 0 1,1 50,0 a 25,25 0 1,1 -50,0"
--- src/components/Artboard.js
+++ src/components/Artboard.js
@@ -1,12 +1,24 @@
-export default function Artboard( { width, height, shapes } ) {
+import styled from "styled-components";
 
-    return <svg
+const StyledArtboard = styled.svg`
+    position: absolute;
+    top: ${ props => props.position.offsetX }%;
+    left: ${ props => props.position.offsetY }%;
+    transform: translate( -50%, -50% );
+    box-shadow: 5px 5px 2px 1px rgba( 0, 0, 0, .2 );
+`;
+
+export default function Artboard( { width, height, position, shapes } ) {
+
+    return <StyledArtboard
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
-        x="0px"
-        y="0px"
-        width={ `${ width }px` }
-        height={ `${ height }px` }
+        className="artboard"
+        x={ 0 }
+        y={ 0 }
+        width={ width * position.zoom }
+        height={ height * position.zoom }
+        position={ position }
         viewBox={ `0 0 ${ width } ${ height }` }
         enableBackground={ `new 0 0 ${ width } ${ height }` }
     >
@@ -19,6 +31,6 @@ export default function Artboard( { width, height, shapes } ) {
                 fill="white"
             />;
         } ) }
-    </svg>;
+    </StyledArtboard>;
 
 }
