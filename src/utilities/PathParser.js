21282fb92985d5faf6f63c2d542baf2c9d9feabf
Josh Frank <eurosewusa@gmail.com>
2021-06-03 18:39:53 -0400

    adding pointGrammar to PathParser

--- src/utilities/PathParser.js
+++ src/utilities/PathParser.js
@@ -18,7 +18,30 @@ class PathParser {
         a: [ this.validCoordinate, this.validCoordinate, this.validCoordinate, this.validFlag, this.validFlag, this.validCoordinate, this.validCoordinate ],
     };
 
-    static components( type, path, cursor ) {
+    static pointGrammar = {
+        z: () => [],
+        Z: () => [],
+        m: ( point, command ) => [ point[ 0 ] + command[ 1 ], point[ 1 ] + command[ 2 ] ],
+        M: ( point, command ) => command.slice( 1 ),
+        h: ( point, command ) => [ point[ 0 ] + command[ 1 ], point[ 1 ] ],
+        H: ( point, command ) => [ command[ 1 ], point[ 1 ] ],
+        v: ( point, command ) => [ point[ 0 ], point[ 1 ] + command[ 1 ] ],
+        V: ( point, command ) => [ point[ 0 ], command[ 1 ] ],
+        l: ( point, command ) => [ point[ 0 ] + command[ 1 ], point[ 1 ] + command[ 2 ] ],
+        L: ( point, command ) => command.slice( 1 ),
+        a: ( point, command ) => [ point[ 0 ] + command[ 6 ], point[ 1 ] + command[ 7 ] ],
+        A: ( point, command ) => command.slice( 6 ),
+        c: ( point, command ) => [ point[ 0 ] + command[ 5 ], point[ 1 ] + command[ 6 ] ],
+        C: ( point, command ) => command.slice( 5 ),
+        t: ( point, command ) => [ point[ 0 ] + command[ 1 ], point[ 1 ] + command[ 2 ] ],
+        T: ( point, command ) => command.slice( 1 ),
+        q: ( point, command ) => [ point[ 0 ] + command[ 3 ], point[ 1 ] + command[ 4 ] ],
+        Q: ( point, command ) => command.slice( 3 ),
+        s: ( point, command ) => [ point[ 0 ] + command[ 3 ], point[ 1 ] + command[ 4 ] ],
+        S: ( point, command ) => command.slice( 3 ),
+    };
+
+    static parseComponents( type, path, cursor ) {
         const expectedCommands = this.pathGrammar[ type.toLowerCase() ];
         const components = [];
         while ( cursor <= path.length ) {
@@ -44,14 +67,14 @@ class PathParser {
         throw new Error( `Invalid path: first error at char ${ cursor }` );
     }
 
-    static parse( path ) {
+    static parseRaw( path ) {
         let cursor = 0, parsedComponents = [];
         while ( cursor < path.length ) {
             const match = path.slice( cursor ).match( this.validCommand );
             if ( match !== null ) {
                 const command = match[ 1 ];
                 cursor += match[ 0 ].length;
-                const componentList = PathParser.components( command, path, cursor );
+                const componentList = PathParser.parseComponents( command, path, cursor );
                 cursor = componentList[ 0 ];
                 parsedComponents = [ ...parsedComponents, ...componentList[1] ];
             } else {
@@ -59,6 +82,14 @@ class PathParser {
             }
         }
         return parsedComponents;
+    }
+
+    static parse( path ) {
+        let currentPoint = [ 0, 0 ];
+        return PathParser.parseRaw( path ).reduce( ( result, command ) => {
+            currentPoint = this.pointGrammar[ command[ 0 ] ]( currentPoint, command );
+            return [ ...result, currentPoint ];
+        }, [] );
     }
 
 }
@@ -72,8 +103,16 @@ export default PathParser;
 //     "m 240 200 v -20 a 20 20 90 1 1 40 40 l -40 -20 z",
 //     "m 80 508 s 70 -54 60 -138 s 66 -98 150 -50 z",
 //     "m 628 356 l 192 -224 l 96 -32 l -32 96 l -224 192 c 32 32 32 64 64 30 c 0 32 32 64 0 64 a 45.44 45.44 90 0 1 -32 32 a 160 160 90 0 0 -64 -96 q -16 -3.2 -16 16 t -48 41.6 t -25.6 -25.6 t 41.6 -48 t 16 -16 a 160 160 90 0 0 -96 -64 a 45.44 45.44 90 0 1 32 -32 c 0 -32 32 0 64 0 c -32 32 0 32 32 66 l 192 -224 l 0 64 l 64 0 l -57.6 -6.4 l -6.4 -57.6 z"
-//   ];
+// ];
 
-// testShapes.forEach( shape => console.log( PathParser.parse( shape ) ) );
+// const sameShape = [
+//     "m 25,25 l 50,0 l 0,50 l -50,0 z",
+//     "M 25 25 L 75 25 L 75 75 L 25 75 Z"
+// ];
 
-// console.log( PathParser.parse( "m l 250 a -400, -350 ." ) );
+// testShapes.forEach( shape => console.log( PathParser.parseRaw( shape ) ) );
+
+// console.log( PathParser.parseRaw( "m l 250 a -400, -350 ." ) );
+
+// console.log( PathParser.parse( sameShape[ 0 ] ) );
+// console.log( PathParser.parse( sameShape[ 1 ] ) );
