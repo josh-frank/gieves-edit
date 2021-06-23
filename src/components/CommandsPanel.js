82e50f8c14b407b3a5e793205d3d6070d35919e1
Josh Frank <eurosewusa@gmail.com>
2021-06-23 18:13:49 -0400

    tweaking styling for CommandsPanel

--- src/components/CommandsPanel.js
+++ src/components/CommandsPanel.js
@@ -1,15 +1,35 @@
 import { useSelector } from "react-redux";
+
+import styled from "styled-components";
+
 import { Path } from "../utilities/PathParser";
 
-const CommandRow = ( { commandLetter, command } ) => <div className="command-row">
-    <input value={ commandLetter } />
-    { command.map( ( parameter, index ) => <input type="number" key={ index } defaultValue={ parameter } /> ) }
+const StyledInput = styled.input.attrs( ( { characterLength } ) => ( {
+    style: { width: `${ characterLength }ch` },
+} ) )``;
+
+const CommandRow = ( { commandLetter, command, dark } ) => <div className="command-row">
+    <StyledInput
+        className={ dark ? "dark" : null }
+        type="text"
+        defaultValue={ commandLetter }
+        characterLength={ 1.5 }
+    />
+    { command.map( ( parameter, index ) => <StyledInput
+        className={ dark ? "dark" : null }
+        type="number"
+        key={ index }
+        defaultValue={ parameter }
+        characterLength={ Math.min( parameter.toString().length, 4 ) }
+    /> ) }
 </div>;
 
 export default function CommandsPanel() {
 
     const { activeShape } = useSelector( state => state.shapes );
 
+    const { dark } = useSelector( state => state.artboard );
+    
     const activePath = activeShape && new Path( activeShape );
 
     const CommandFields = () => activePath && <section className="command-fields">
@@ -17,6 +37,7 @@ export default function CommandsPanel() {
             key={ index }
             commandLetter={ parsedCommand.commandLetter }
             command={ parsedCommand.commandLetter === parsedCommand.commandLetter.toLowerCase() ? parsedCommand.relativeCommand : parsedCommand.absoluteCommand }
+            dark={ dark }
         /> ) }
     </section>;
 
