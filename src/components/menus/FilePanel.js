001fd4da8ac32ae946dc1e9ae280cc78aad92612
Josh Frank <eurosewusa@gmail.com>
2021-07-05 14:45:13 -0400

    added file utility to parse paths from an SVG file - fixed bug with null paths in SVG download

--- src/components/menus/FilePanel.js
+++ src/components/menus/FilePanel.js
@@ -1,16 +1,20 @@
 import { useSelector } from "react-redux";
 
-import { downloadPathsAsSvgFile } from "../../utilities/pathsToSvgFile";
+import { downloadPathsAsSvgFile } from "../../utilities/fileUtilities";
 
 export default function FilePanel() {
 
     const { activeShape, inactiveShapes } = useSelector( state => state.shapes );
     const { width, height } = useSelector( state => state.artboard );
 
+    const onDownload = () => {
+        downloadPathsAsSvgFile( activeShape ? [ activeShape, ...inactiveShapes ] : [ ...inactiveShapes ], [ width, height ] );
+    };
+
     return <div className="menu-panel">
         <div className="menu-header">File</div>
         <button
-            onClick={ () => downloadPathsAsSvgFile( [ activeShape, ...inactiveShapes ], [ width, height ] ) }
+            onClick={ onDownload }
         >
             〈／〉 <b>Download as SVG</b>
         </button>
--- src/utilities/fileUtilities.js
+++ src/utilities/fileUtilities.js
@@ -1,4 +1,4 @@
-// export const pathsToSvgFile = ( paths, dimensions ) => [
+// export const pathsToSvg = ( paths, dimensions ) => [
 //     `<?xml version="1.0" encoding="utf-8"?>`,
 //     `<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">`,
 //     `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="${ dimensions[ 0 ] }px" height="${ dimensions[ 1 ] }px" viewBox="0 0 ${ dimensions[ 0 ] } ${ dimensions[ 1 ] }" enable-background="new 0 0 ${ dimensions[ 0 ] } ${ dimensions[ 1 ] }" xml:space="preserve">`,
@@ -20,4 +20,15 @@ export const downloadPathsAsSvgFile = ( paths, dimensions ) => {
     document.body.appendChild( element );
     element.click();
     document.body.removeChild( element );
+}
+
+export const parsePathsFromSvgFile = svg => {
+    const matchPathTag = /<path[\s\S]*?\/>/gmi,
+        matchDescriptor = /(?<=\sd=")[\s\S]*?(?=")/gmi,
+        cleanUpWhiteSpace = /[\n\s]+/g;
+    return svg.match( matchPathTag )
+        .map( pathTag => pathTag.match( matchDescriptor )
+            .join( "" )
+            .replace( cleanUpWhiteSpace, " " )
+        );
 }
